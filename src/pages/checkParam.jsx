a20cb623655b3ed7abd651087df16c6d884fdafa
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2022-12-15 16:10:57 +0530

    Updated

--- src/pages/checkParam.jsx
+++ src/pages/checkParam.jsx
@@ -1,4 +1,4 @@
-import { Component, Fragment } from "react";
+import { Component } from "react";
 import Popup from 'reactjs-popup';
 
 import { useSearchParams } from 'react-router-dom';
@@ -7,16 +7,19 @@ import { v4 as uuid } from 'uuid';
 
 function CheckParam() {
     const [searchParams] = useSearchParams();
-    console.log(searchParams.get('status'));
-    if (searchParams.has('status')) {
-        if (searchParams.get('status') === 'failure') {
-            alert('Payment failed Retry Again');
-            window.location = "https://ultron-official.netlify.app/";
-        }
-        else {
-            alert('Payment Success');
-            // window.location = "https://6396e89fd3b1f2587021ba76--chipper-croquembouche-37664d.netlify.app/";
-        }
+    console.log(searchParams.get('OrderID'));
+    if (searchParams.has('OrderID')) {
+
+        alert(searchParams.get('OrderID'));
+        window.location = "/pass?OrderID=order_Ks6jb8TYdShwi8&bgurl=https://payment.ultronofficial.online/pass1.jpeg";
+        // if (searchParams.get('status') === 'failure') {
+        //     alert('Payment failed Retry Again');
+        //     window.location = "https://ultron-official.netlify.app/";
+        // }
+        // else {
+        //     alert('Payment Success');
+        //     // window.location = "https://6396e89fd3b1f2587021ba76--chipper-croquembouche-37664d.netlify.app/";
+        // }
   }  
   }
   
